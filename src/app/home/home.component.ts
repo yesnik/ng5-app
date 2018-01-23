50859d0d41211277b4636898121e684e07e7c00a
Nikita Nyatin <yesnik@yandex.ru>
2018-01-23 08:00:43 +0500

    Add new goals to the list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = 'Add an item';
+  goalText: string = 'My super goal';
+  goals: array = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.updateItemCount();
   }
 
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.updateItemCount();
+  }
+
+  updateItemCount() {
+    this.itemCount = this.goals.length;
+  }
 }
