8fb068f90d71b7e18b2e2e6d211b962a1f89c5f5
Nikita Nyatin <yesnik@yandex.ru>
2018-01-24 07:57:32 +0500

    Add remove animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,14 @@ import { trigger, style, transition, animate, keyframes, query, stagger
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1})
           ]))
+        ]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))
         ]), {optional: true})
       ])
     ])
@@ -42,5 +50,9 @@ export class HomeComponent implements OnInit {
 
   updateItemCount() {
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 }
